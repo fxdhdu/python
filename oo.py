caf40a04cbc26289267b3966e0c7e4436d6f7163
fxd <757866311@qq.com>
2016-06-23 19:44:30 +0800

    oo

---
+++ oo.py
@@ -0,0 +1,21 @@
+class Hello:
+
+	def __init__(self, name):
+		self._name = name
+
+	def sayHello(self):
+		print("Hello {0}".format(self._name))
+
+class Hi(Hello):
+
+	def __init__(self, name):
+		Hello.__init__(self, name)
+
+	def sayHi(self):
+		print("Hi {0}".format(self._name))
+
+h = Hello("jikexueyuan")
+h.sayHello()
+
+h1 = Hi("hangzhou")
+h1.sayHi()
