ef61cf615333920e33189fe1beb9acdd2ffef398
fxd <757866311@qq.com>
2016-06-24 15:40:55 +0800

    数与字符串

---
+++ 2-8.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+print "hello boy\nhello boy"
+print r"hello boy\nhello boy" #自然字符串
+
+print "hello girl\n"*20
+
+c1 = "0123456789"
+c2 = c1[0]
+c3 = c1[7]
+c4 = c1[:2]
+c5 = c1[2:]
+c6 = c1[4:7] #切片运算符
+print c1
+print c2
+print c3
+print c4
+print c5
+print c6
